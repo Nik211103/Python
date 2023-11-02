389703f7550cbc050a2f4eb3f3fa3c837115bc1c
Nikita <mitichkin0303@mail.ru>
2023-11-03 00:11:49 +0200

    Балуюсь с компютерным зрением

---
+++ Ex/Ex003.py
@@ -0,0 +1,24 @@
+﻿""""
+input_list = "a a a b c a a d c d d"
+user_list = input_list.split()
+
+result_list = []
+
+for i in range(len(user_list)):
+    tmp = user_list[:i].count(user_list[i])
+    if tmp >= 1:
+        result_list.append(f'{user_list[i]}_{tmp}')
+    else:
+        result_list.append(user_list[i])
+    
+print(input_list)
+print(" ".join(result_list))
+"""
+
+
+
+
+
+
+
+        
---
+++ Ex/app.py
@@ -0,0 +1,21 @@
+﻿from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "<h1>Добрый вечер, уважаемые студенты!</h1>" \
+"<a href='/info'>Обо мне</a><br>" \
+"<a href='/calc/1/5'>Калькулятор</a>"
+
+@app.route("/info")
+def info():
+    return "<b>Меня создала компания GeekBrains!</b>"
+
+@app.route("/calc/<x>/<y>")
+def calc(x, y):
+    return f"{x} + {y} = {int(x) + int(y)}"
+
+
+if __name__ == '__main__':
+    app.run()
